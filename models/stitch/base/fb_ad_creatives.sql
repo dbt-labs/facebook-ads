d591b18e0d10fd84d57b3dee0d07c9e38a783a6a
Drew Banin <drew@8tracks.com>
2016-11-04 11:16:20 -0400

    misplaced comma

--- models/stitch/base/fb_ad_creatives.sql
+++ models/stitch/base/fb_ad_creatives.sql
@@ -5,9 +5,9 @@ with base as (
     url_tags,
     coalesce(
       nullif(object_story_spec__link_data__call_to_action__value__link, ''),
-      nullif(object_story_spec__video_data__call_to_action__value__link, '')
+      nullif(object_story_spec__video_data__call_to_action__value__link, ''),
       nullif(object_story_spec__link_data__link, ''),
-      nullif(link_url, ''),
+      nullif(link_url, '')
     ) as url
   from
     {{ var('ad_creatives_table') }}
