60ee7c501eb2c6dd8d3b6e36db5304c6d22b65bd
Tristan Handy <jthandy@gmail.com>
2016-12-11 14:31:45 -0500

    updated creatives table

--- models/stitch/base/fb_ad_creatives.sql
+++ models/stitch/base/fb_ad_creatives.sql
@@ -2,7 +2,6 @@ with base as (
 
   select
     id,
-    lower(url_tags) as url_tags,
     lower(coalesce(
       nullif(object_story_spec__link_data__call_to_action__value__link, ''),
       nullif(object_story_spec__video_data__call_to_action__value__link, ''),
@@ -13,14 +12,21 @@ with base as (
   from
     {{ var('ad_creatives_table') }}
 
+), splits as (
+
+  select
+    *,
+    split_part(url ,'?', 1) as base_url,
+    split_part(url ,'?', 2) as url_tags
+  from base
+
 )
 
 select
   *,
-  split_part(url ,'?',1) as base_url,
   split_part(split_part(url_tags,'utm_source=',2), '&', 1) as utm_source,
   split_part(split_part(url_tags,'utm_medium=',2), '&', 1) as utm_medium,
   split_part(split_part(url_tags,'utm_campaign=',2), '&', 1) as utm_campaign,
   split_part(split_part(url_tags,'utm_content=',2), '&', 1) as utm_content,
   split_part(split_part(url_tags,'utm_term=',2), '&', 1) as utm_term
-from base
+from splits
