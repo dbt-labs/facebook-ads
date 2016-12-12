5b99b234721600828aee14052195b1b407c8d037
Tristan Handy <jthandy@gmail.com>
2016-12-12 11:42:03 -0500

    minor update to creatives table

--- models/stitch/base/fb_ad_creatives.sql
+++ models/stitch/base/fb_ad_creatives.sql
@@ -2,7 +2,7 @@ with base as (
 
   select
     id,
-    nullif(url_tags, ''),
+    lower(nullif(url_tags, '')) as url_tags,
     lower(coalesce(
       nullif(object_story_spec__link_data__call_to_action__value__link, ''),
       nullif(object_story_spec__video_data__call_to_action__value__link, ''),
