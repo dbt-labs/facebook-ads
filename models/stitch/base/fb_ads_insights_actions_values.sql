cf8d9ba872b83793044ddc7c4ffb0f79bf2af825
Erin Ogilvy <erin.ogilvy@gmail.com>
2017-06-15 10:20:20 -0400

    adding null checks for all varchar values

--- models/stitch/base/fb_ads_insights_actions_values.sql
+++ models/stitch/base/fb_ads_insights_actions_values.sql
@@ -6,11 +6,11 @@ with actions as (
 
 select
   _sdc_level_0_id as id,
-  _sdc_source_key_ad_id as ad_id,
-  _sdc_source_key_adset_id as adset_id,
-  _sdc_source_key_campaign_id as campaign_id,
+  nullif(_sdc_source_key_ad_id,'') as ad_id,
+  nullif(_sdc_source_key_adset_id,'') as adset_id,
+  nullif(_sdc_source_key_campaign_id,'') as campaign_id,
   _sdc_source_key_date_start::date as date_day,
-  action_destination,
-  action_type,
+  nullif(action_destination,'') as action_destination,
+  nullif(action_type,'') as action_type,
   value as action_value
 from actions
