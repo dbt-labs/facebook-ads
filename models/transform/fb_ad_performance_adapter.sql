9ed95662a536a73adb869127e250e01a32ed48a4
Alexa Diaz <axela.zaid@gmail.com>
2018-11-30 10:03:59 -0500

    updated fields

--- models/transform/fb_ad_performance_adapter.sql
+++ models/transform/fb_ad_performance_adapter.sql
@@ -8,6 +8,9 @@ fb_keyword_performance_agg as (
 
     select 
         date_day as campaign_date,
+        adset_id as ad_group_id,
+        adset_name as ad_group_name,
+        campaign_id,
         url_host,
         url_path,
         utm_source,
@@ -15,12 +18,13 @@ fb_keyword_performance_agg as (
         utm_campaign,
         utm_content,
         utm_term,
+        campaign_name,
         'facebook ads' as platform,
         sum(clicks) as clicks,
         sum(impressions) as impressions,
         sum(spend) as spend
     from fb_keyword_performance
-    group by 1, 2, 3, 4, 5, 6, 7, 8, 9
+    group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13
 
 )
 
