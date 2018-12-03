012d3911b9b8b9bbf9a42bb956a2faf31ff8eb34
Alexa Diaz <axela.zaid@gmail.com>
2018-12-03 16:31:29 -0500

    updated files with to reference dbt_utils when necessary

--- macros/stitch/base/stitch_fb_ads_adsets.sql
+++ macros/stitch/base/stitch_fb_ads_adsets.sql
@@ -7,14 +7,15 @@
 
 {% macro default__stitch_fb_ads_adsets() %}
 
-select
-  id,
-  nullif(name,'') as name,
-  nullif(account_id,'') as account_id,
-  nullif(campaign_id,'') as campaign_id,
-  created_time,
-  nullif(effective_status,'') as effective_status
-from
-  {{ var('adsets_table') }}
+    select
+    
+        id,
+        nullif(name,'') as name,
+        nullif(account_id,'') as account_id,
+        nullif(campaign_id,'') as campaign_id,
+        created_time,
+        nullif(effective_status,'') as effective_status
+        
+    from {{ var('adsets_table') }}
 
 {% endmacro %}
--- models/transform/fb_ads_xf.sql
+++ models/transform/fb_ads_xf.sql
@@ -25,6 +25,6 @@ with ads_xf_ads as (
             when version_number = num_versions then null
             else lead(updated_at) over (partition by id order by updated_at)
         end as effective_to,
-        md5(id || '|' || version_number) as unique_id
+        {{ dbt_utils.surrogate_key('id', 'version_number') as unique_id
 
 from ads_xf_windowed
