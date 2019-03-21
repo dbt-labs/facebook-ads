3f7fb1a2bd3e65c09737ac02b7ad74d12b22dce4
Chris Lawliss <chrislawliss@gmail.com>
2019-03-21 15:15:45 -0400

    reformat url_tags for readbility

--- macros/fivetran/base/fivetran_fb_url_tag.sql
+++ macros/fivetran/base/fivetran_fb_url_tag.sql
@@ -25,99 +25,35 @@ pivot as (
 
         creative_id,
     
-        {% for key_value in [
-        ['affcode','url_host'],
-        ['utm_source','utm_source'],
-        ['utm_medium','utm_medium'],
-        ['utm_campaign','utm_campaign'],
-        ['utm_content','utm_content'],
-        ['utm_term','utm_term']
-        ]%}
+        {% for key, value in {
+        "affcode": "url_host",
+        "utm_source": "utm_source",
+        "utm_medium": "utm_medium",
+        "utm_campaign": "utm_campaign",
+        "utm_content": "utm_content",
+        "utm_term": "utm_term"
+        }.items()%}
 
-        first_value(
-            case 
-                when key = '{{key_value[0]}}' then value
-                else null  
-            end
-            ignore nulls
-        ) over (
-            partition by creative_id
-            order by key
-        ) as {{key_value[1]}}
+            first_value(
+                case 
+                    when key = '{{key}}' then value
+                    else null  
+                end
 
-        {% if not loop.last %}    
-        ,
-        {% endif %} 
+                {% if target.type == 'snowflake' %}
+                ) ignore nulls 
+                {% else %}
+                ignore nulls )
+                {% endif %}
 
-        {% endfor %}
+            over (
+                partition by creative_id
+                order by key
+            ) as {{value}}
 
-    from base
-
-),
-
-final as (
-
-    select 
-        creative_id,
-        url_host,
-        utm_source,
-        utm_medium,
-        utm_campaign,
-        utm_content,
-        utm_term
-
-    from pivot
-    {{dbt_utils.group_by(7)}}
-)
-
-select * from final
-
-{% endmacro %}
-
-
-{% macro snowflake__fivetran_fb_url_tag() %}
-
-with base as (
-    
-select distinct
-    type,
-    key,
-    creative_id,
-    value
-
-from
-    {{ var('url_tag_table') }}
-  
-),
-
-pivot as (
-    
-    select 
-
-        creative_id,
-    
-        {% for key_value in [
-        ['affcode','url_host'],
-        ['utm_source','utm_source'],
-        ['utm_medium','utm_medium'],
-        ['utm_campaign','utm_campaign'],
-        ['utm_content','utm_content'],
-        ['utm_term','utm_term']
-        ]%}
-
-        first_value(
-            case 
-                when key = '{{key_value[0]}}' then value
-                else null  
-            end
-        ) ignore nulls over (
-            partition by creative_id
-            order by key
-        ) as {{key_value[1]}}
-
-        {% if not loop.last %}    
-        ,
-        {% endif %} 
+            {% if not loop.last %}    
+            ,
+            {% endif %} 
 
         {% endfor %}
 
@@ -138,7 +74,6 @@ final as (
 
     from pivot
     {{dbt_utils.group_by(7)}}
-
 )
 
 select * from final
