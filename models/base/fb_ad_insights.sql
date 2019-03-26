7ee9cf8a77a4239ec0d5a503ec740e68206f852b
Chris Lawliss <chrislawliss@gmail.com>
2019-03-26 11:32:55 -0400

    removed models from base that were ported to router based models

--- models/base/fb_ad_insights.sql
+++
@@ -1,9 +0,0 @@
-{% if var('etl') == 'stitch' %}
-
-{{ stitch_fb_ad_insights() }}
-
-{% elif var('etl') == 'fivetran'%}
-
-{{ fivetran_fb_ad_insights() }}
-
-{% endif %}
--- models/base/fb_ads.sql
+++
@@ -1,9 +0,0 @@
-{% if var('etl') == 'stitch' %}
-
-{{ stitch_fb_ads() }}
-
-{% elif var('etl') == 'fivetran'%}
-
-{{ fivetran_fb_ads() }}
-
-{% endif %}
