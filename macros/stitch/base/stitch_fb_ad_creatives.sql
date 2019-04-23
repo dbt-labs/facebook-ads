1c7d498389275f866030160805ffd5861574a06a
Ken-Lo <ken@kenstechtips.com>
2019-04-23 14:52:34 +0100

    Double underscore to dot

--- macros/stitch/base/stitch_fb_ad_creatives.sql
+++ macros/stitch/base/stitch_fb_ad_creatives.sql
@@ -14,9 +14,9 @@ with base as (
         id as creative_id,
         lower(nullif(url_tags, '')) as url_tags,
         lower(coalesce(
-          nullif(object_story_spec__link_data__call_to_action__value__link, ''),
-          nullif(object_story_spec__video_data__call_to_action__value__link, ''),
-          nullif(object_story_spec__link_data__link, '')
+          nullif(object_story_spec.link_data.call_to_action.value.link, ''),
+          nullif(object_story_spec.video_data.call_to_action.value.link, ''),
+          nullif(object_story_spec.link_data.link, '')
         )) as url
     
     from
