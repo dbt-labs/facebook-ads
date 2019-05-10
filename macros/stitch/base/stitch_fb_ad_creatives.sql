7b55c9ec61792d0798864304a858e3529b6810cd
Alexa Diaz <axela.zaid@gmail.com>
2019-05-10 10:23:08 -0400

    add nested field macro and cleaned up fb_ad_creatives model

--- macros/stitch/base/stitch_fb_ad_creatives.sql
+++ macros/stitch/base/stitch_fb_ad_creatives.sql
@@ -24,81 +24,23 @@ links_joined as (
     select
     
         id as creative_id,
-        lower(nullif(url_tags, '')) as url_tags,
+        
+        
         lower(coalesce(
           nullif(child_link, ''),
-          nullif(object_story_spec__link_data__call_to_action__value__link, ''),
-          nullif(object_story_spec__video_data__call_to_action__value__link, ''),
-          nullif(object_story_spec__link_data__link, '')
-        )) as url
-    
-    from base
-    left join child_links 
-        on base.id = child_links.creative_id
-    
-), 
-
-splits as (
-
-    select
-    
-        links_joined.creative_id,
-        links_joined.url,
-        {{ dbt_utils.split_part('url', "'?'", 1) }} as base_url,
-        --this is a strange thing to have to do but it's because sometimes 
-        --the URL exists on the story object and we wouldn't get the appropriate 
-        --UTM params here otherwise
-        coalesce(links_joined.url_tags, {{ dbt_utils.split_part('url', "'?'", 2) }} ) as url_tags
-    
-    from links_joined
-
-)
-
-select
-
-    splits.*,
-    {{ dbt_utils.get_url_host('url') }} as url_host,
-    '/' || {{dbt_utils.get_url_path('url') }} as url_path,
-    
-    {{ facebook_ads.get_url_parameter() }}
-    
-from splits
-
-{% endmacro %}
-
-
-{% macro snowflake__stitch_fb_ad_creatives() %}
-
-with base as (
-    
-    select * from {{ var('ad_creatives_table') }}
-    
-),
-
-child_links as (
-    
-    select * from {{ ref('fb_ad_creatives__child_links') }}
-    
-),
-
-links_joined as (
-    
-    select
-
-        id as creative_id,
-        lower(coalesce(
-            nullif(child_link, ''),
-            nullif(base.object_story_spec['link_data']['call_to_action']['value']['link']::varchar, ''),
-            nullif(base.object_story_spec['video_data']['call_to_action']['value']['link']::varchar, ''),
-            nullif(base.object_story_spec['link_data']['link']::varchar, '')
+          nullif({{ facebook_ads.nested_field('base.object_story_spec', ['link_data', 'call_to_action', 'value', 'link']) }}, ''),
+          nullif({{ facebook_ads.nested_field('base.object_story_spec', ['video_data', 'call_to_action', 'value', 'link']) }}, ''),
+          nullif({{ facebook_ads.nested_field('base.object_story_spec', ['link_data', 'link']) }}, '')
         )) as url,
-        
-        url_tags
-        
+      
+        lower(coalesce(
+            nullif(url_tags, {{ dbt_utils.split_part('url', "'?'", 2) }}), '')
+        ) as url_tags
+    
     from base
     left join child_links 
         on base.id = child_links.creative_id
-        
+    
 ),
 
 parsed as (
@@ -106,11 +48,10 @@ parsed as (
     select
     
         links_joined.*,
-        creative_id,
-        url,
         {{ dbt_utils.split_part('url', "'?'", 1) }} as base_url,
         {{ dbt_utils.get_url_host('url') }} as url_host,
-        '/' || {{dbt_utils.get_url_path('url') }} as url_path,
+        
+        {{ dbt_utils.concat(["'/'", dbt_utils.get_url_path('url')]) }} as url_path,
         
         {{ facebook_ads.get_url_parameter() }}
         
